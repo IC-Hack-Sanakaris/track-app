5797dd9af976de31b06b8f0e7195d111634b6566
debz-g <debz.exe@gmail.com>
2022-09-03 15:03:17 +0530

    Added Login and Signup screens 2

--- app/src/main/java/dev/refox/trackapp/MainActivity.kt
+++ app/src/main/java/dev/refox/trackapp/MainActivity.kt
@@ -2,10 +2,16 @@ package dev.refox.trackapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.refox.trackapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bind: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
     }
 }
--- app/src/main/java/dev/refox/trackapp/auth/LoginActivity.kt
+++ app/src/main/java/dev/refox/trackapp/auth/LoginActivity.kt
@@ -4,18 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.refox.trackapp.R
+import dev.refox.trackapp.databinding.ActivityLoginBinding
 import dev.refox.trackapp.databinding.ActivitySignupBinding
 
-class SignupActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySignupBinding
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignupBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvToLogin.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+        binding.tvToSignup.setOnClickListener {
+            startActivity(Intent(this, SignupActivity::class.java))
         }
     }
 }
