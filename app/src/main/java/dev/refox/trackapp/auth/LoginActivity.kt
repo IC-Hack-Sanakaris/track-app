a986cffcbf065ae1f977e9c3f26047f5ebe6ef3e
madhurmehta007 <12madhurmehta@gmail.com>
2022-09-03 15:44:46 +0530

    LogIn and SignUp Implemented using Firebase

--- app/src/main/java/dev/refox/trackapp/auth/LoginActivity.kt
+++ app/src/main/java/dev/refox/trackapp/auth/LoginActivity.kt
@@ -3,6 +3,9 @@ package dev.refox.trackapp.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import dev.refox.trackapp.MainActivity
 import dev.refox.trackapp.R
 import dev.refox.trackapp.databinding.ActivityLoginBinding
 import dev.refox.trackapp.databinding.ActivitySignupBinding
@@ -10,13 +13,48 @@ import dev.refox.trackapp.databinding.ActivitySignupBinding
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        firebaseAuth = FirebaseAuth.getInstance()
         binding.tvToSignup.setOnClickListener {
-            startActivity(Intent(this, SignupActivity::class.java))
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if(firebaseAuth.currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/dev/refox/trackapp/auth/SignupActivity.kt
+++ app/src/main/java/dev/refox/trackapp/auth/SignupActivity.kt
@@ -3,19 +3,49 @@ package dev.refox.trackapp.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import dev.refox.trackapp.R
 import dev.refox.trackapp.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignupBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         binding.tvToLogin.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnSignup.setOnClickListener {
+            val email = binding.emailET.text.toString()
+            val name = binding.nameEt.text.toString()
+            val confirmPass = binding.confirmPassEt.text.toString()
+
+            if (email.isNotEmpty() && name.isNotEmpty() && confirmPass.isNotEmpty()) {
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, confirmPass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
         }
     }
 }
