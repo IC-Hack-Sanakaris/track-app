2d3ed0a03f59eea1a0f4f102b667047fa517373b
debz-g <debz.exe@gmail.com>
2022-09-03 15:01:26 +0530

    Added Login and Signup screens

---
+++ app/src/main/java/dev/refox/trackapp/auth/SignupActivity.kt
@@ -0,0 +1,12 @@
+package dev.refox.trackapp.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.refox.trackapp.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
